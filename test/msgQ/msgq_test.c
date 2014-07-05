905bb108108102dba931ff4782d4970b6230cccd
fritzprix <innocentevil0914@gmail.com>
2014-07-06 02:06:30 +0900

    - msgq test routine

---
+++ test/msgQ/msgq_test.c
@@ -0,0 +1,71 @@
+/*
+ * msgq_test.c
+ *
+ *  Created on: 2014. 7. 6.
+ *      Author: innocentevil
+ */
+
+
+
+#include "msgq_test.h"
+
+static DECLARE_THREADROUTINE(sender);
+static DECLARE_THREADSTACK(sender_stack,1 << 8);
+static tch_thread_id sender_id;
+
+static DECLARE_THREADROUTINE(receiver);
+static DECLARE_THREADSTACK(receiver_stack,1 << 8);
+static tch_thread_id receiver_id;
+
+static tch_msgQDef(test,10);
+
+static tch_msgQue_id mid;
+
+
+osStatus do_msgqBaseTest(tch* api){
+
+	tch_msgq_ix* MsgQ = api->MsgQ;
+	mid = MsgQ->create(&msgQ_test);
+
+	tch_thread_ix* Thread = api->Thread;
+	tch_thread_cfg tcfg;
+	tcfg._t_name = "sender";
+	tcfg._t_routine = sender;
+	tcfg._t_stack = sender_stack;
+	tcfg.t_proior = Normal;
+	tcfg.t_stackSize = 1 << 8;
+	sender_id = Thread->create(&tcfg,api);
+
+	tcfg._t_name = "receiver";
+	tcfg._t_routine = receiver;
+	tcfg._t_stack = receiver_stack;
+	receiver_id = Thread->create(&tcfg,api);
+
+	Thread->start(receiver_id);
+	Thread->start(sender_id);
+	return api->Thread->join(receiver_id,osWaitForever);
+
+}
+
+
+DECLARE_THREADROUTINE(sender){
+	tch* api = (tch*) arg;
+	uint32_t cnt = 0;
+	while(cnt < 100){
+		api->Thread->sleep(20);
+		api->MsgQ->put(mid,0xFF,osWaitForever);
+		cnt++;
+	}
+	return NULL;
+}
+
+DECLARE_THREADROUTINE(receiver){
+	tch* api = (tch*) arg;
+	uint32_t cnt = 0;
+	while(cnt < 100){
+		osEvent evt = api->MsgQ->get(mid,osWaitForever);
+		if(evt.status == osOK)
+			cnt++;
+	}
+	return NULL;
+}
---
+++ test/msgQ/msgq_test.h
@@ -0,0 +1,16 @@
+/*
+ * msgq_test.h
+ *
+ *  Created on: 2014. 7. 6.
+ *      Author: innocentevil
+ */
+
+#ifndef MSGQ_TEST_H_
+#define MSGQ_TEST_H_
+
+#include "tch.h"
+
+extern osStatus do_msgqBaseTest(tch* api);
+
+
+#endif /* MSGQ_TEST_H_ */
