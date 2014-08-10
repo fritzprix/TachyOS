3b6522acec645eb83f31598872828178c789ac91
fritzprix <innocentevil0914@gmail.com>
2014-08-11 00:58:27 +0900

    - fix Async for handling error condition

--- source/sys/tch_async.c
+++ source/sys/tch_async.c
@@ -20,6 +20,7 @@
 #define TCH_ASYNC_STATUS_ONQUEUE_BLK  ((uint8_t) 2)
 #define TCH_ASYNC_STATUS_ONQUEUE_NBLK ((uint8_t) 4)
 #define TCH_ASYNC_STATUS_NOTIFIED     ((uint8_t) 8)
+#define TCH_ASYNC_STATUS_DESTROY      ((uint8_t) 16)
 
 #define TCH_ASYNC_STATUS_ONQUEUE      (TCH_ASYNC_STATUS_ONQUEUE_BLK | TCH_ASYNC_STATUS_ONQUEUE_NBLK)
 
@@ -59,7 +60,6 @@ static tch_async_id tch_async_create(tch_async_routine fn,void* arg,uint8_t prio
 	tch_async_cb* cb = (tch_async_cb*) malloc(sizeof(tch_async_cb));
 	cb->arg = arg;
 	cb->fn = fn;
-	cb->own = Thread->self();
 	tch_listInit((tch_lnode_t*)&cb->wq);
 	tch_listInit(&cb->ln);
 
@@ -68,7 +68,7 @@ static tch_async_id tch_async_create(tch_async_routine fn,void* arg,uint8_t prio
 }
 
 static tchStatus tch_async_start(tch_async_id id){
-	if(((tch_async_cb*)id)->own != Thread->self())
+	if(GET_STATUS(id) & TCH_ASYNC_STATUS_ONQUEUE)
 		return osErrorOS;
 	SET_STATUS(id,TCH_ASYNC_STATUS_ONQUEUE_BLK);
 	if(__get_IPSR()){
@@ -81,9 +81,9 @@ static tchStatus tch_async_start(tch_async_id id){
 }
 
 static tchStatus tch_async_blockedstart(tch_async_id id,uint32_t timeout){
-	tchStatus result = osOK;
-	if(((tch_async_cb*)id)->own != Thread->self())
+	if(GET_STATUS(id) & TCH_ASYNC_STATUS_ONQUEUE)
 		return osErrorOS;
+	tchStatus result = osOK;
 	SET_STATUS(id,TCH_ASYNC_STATUS_ONQUEUE_BLK);
 	if(__get_IPSR()){
 		return osErrorISR;
@@ -95,6 +95,8 @@ static tchStatus tch_async_blockedstart(tch_async_id id,uint32_t timeout){
 }
 
 static void tch_async_notify(tch_async_id id,tchStatus res){
+	if(!(GET_STATUS(id) & TCH_ASYNC_STATUS_ONQUEUE))
+		return;
 	switch(GET_STATUS(id)){
 	case TCH_ASYNC_STATUS_ONQUEUE_BLK:
 		if(__get_IPSR()){
@@ -114,10 +116,9 @@ static void tch_async_notify(tch_async_id id,tchStatus res){
 
 
 static void tch_async_destroy(tch_async_id id){
-	if(((tch_async_cb*)id)->own != Thread->self())
-		return;
 	while(GET_STATUS(id) & TCH_ASYNC_STATUS_ONQUEUE)
 		Thread->sleep(0);                                          // yield wait until async is possible to be destroied
+	SET_STATUS(id,TCH_ASYNC_STATUS_DESTROY);
 	free((void*)id);
 }
 
