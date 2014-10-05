78d771aea5b16a8739f08561e3e015c70eee6cdc
fritzprix <innocentevil0914@gmail.com>
2014-10-05 23:21:34 +0900

    - sync source tree

---
+++ include/test/hal/uart_test.h
@@ -0,0 +1,22 @@
+/*
+ * uart_test.h
+ *
+ *  Created on: 2014. 10. 5.
+ *      Author: innocentevil
+ */
+
+#ifndef UART_TEST_H_
+#define UART_TEST_H_
+
+#if defined(__cplusplus)
+extern "C"{
+#endif
+
+extern tchStatus uart_performTest(tch* api);
+
+#if defined(__cplusplus)
+}
+#endif
+
+
+#endif /* UART_TEST_H_ */
---
+++ source/test/hal/uart_test.c
@@ -0,0 +1,13 @@
+/*
+ * uart_test.c
+ *
+ *  Created on: 2014. 10. 5.
+ *      Author: innocentevil
+ */
+
+#include "tch.h"
+#include "uart_test.h"
+
+tchStatus uart_performTest(tch* api){
+
+}
