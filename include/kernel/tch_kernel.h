a2dbffc3d0dbae290a5274f423f67fc22fa5cd1c
user.name <innocentevil0914@gmail.com>
2015-06-11 06:25:37 +0900

    <*> working on memory management for allocation of discrete sized chuck for mmu-less hardware  <*> fix macros for handling embedded struct offset to prevent from conflict with predefined ones

--- include/kernel/tch_kernel.h
+++ include/kernel/tch_kernel.h
@@ -34,10 +34,13 @@
 #define __BUILD_TIME_EPOCH 0UL
 #endif
 
-
+#ifndef offsetof
 #define offsetof(type,member)		((type*) 0)->member
+#endif
 
+#ifndef container_of
 #define container_of(ptr,type,member) 		 (((size_t) ptr - (size_t) offsetof(type,member)))
+#endif
 
 #define TCH_SYS_TASKQ_SZ                    (16)
 #define DECLARE_SYSTASK(fn)                  void fn(int id,const tch* env,void* arg)
