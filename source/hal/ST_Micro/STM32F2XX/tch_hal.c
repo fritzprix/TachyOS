283a3b3e21367a15528843f35d70e3918048bcf9
fritzprix <innocentevil0914@gmail.com>
2015-07-15 19:17:19 +0900

    + section type was unspecified in default memory configuration    so some part of data section was corrupted

--- source/hal/ST_Micro/STM32F2XX/tch_hal.c
+++ source/hal/ST_Micro/STM32F2XX/tch_hal.c
@@ -1035,28 +1035,28 @@ void tch_hal_setSleepMode(tch_lplvl lplvl){
 
 const struct section_descriptor __default_sections[] = {
 		{		// kernel dynamic section
-				.flags = TYPE_INRAM,
+				.flags = (TYPE_INRAM | SECTION_NORMAL),
 				.start = &_skheap,
 				.end = &_ekheap
 		},
 		{
 				// kernel text section
-				.flags = TYPE_INROM,
+				.flags = (TYPE_INROM | SECTION_KERNEL),
 				.start = &_stext,
 				.end = &_etext
 		},
 		{		// kernel bss section (zero filled data)
-				.flags = TYPE_INRAM,
+				.flags = (TYPE_INRAM | SECTION_KERNEL),
 				.start = &_sbss,
 				.end = &_ebss
 		},
 		{		// kernel data section (initialized to specified value)
-				.flags = TYPE_INRAM,
+				.flags = (TYPE_INRAM | SECTION_KERNEL),
 				.start = &_sdata,
 				.end = &_edata
 		},
 		{		// kernel stack
-				.flags = TYPE_INRAM,
+				.flags = (TYPE_INRAM | SECTION_KERNEL),
 				.start = &_sstack,
 				.end = &_estack
 		}
