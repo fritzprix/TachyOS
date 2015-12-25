ed7f6b6e5ef9368aed30340aeefa29d51688118e
user.name <innocentevil0914@gmail.com>
2015-12-25 11:31:13 +0900

    small fix in hal recipe

--- include/hal/ST_Micro/STM32F2XX/stm32f2xx.h
+++ include/hal/ST_Micro/STM32F2XX/stm32f2xx.h
@@ -12,15 +12,15 @@
   *          is using in the C source code, usually in main.c. This file contains:
   *           - Configuration section that allows to select:
   *              - The device used in the target application
-  *              - To use or not the peripheral’s drivers in application code(i.e.
-  *                code will be based on direct access to peripheral’s registers 
+  *              - To use or not the peripheralï¿½s drivers in application code(i.e.
+  *                code will be based on direct access to peripheralï¿½s registers 
   *                rather than drivers API), this option is controlled by 
   *                "#define USE_STDPERIPH_DRIVER"
   *              - To change few application-specific parameters such as the HSE 
   *                crystal frequency
   *           - Data structures and the address mapping for all peripherals
   *           - Peripheral's registers declarations and bits definition
-  *           - Macros to access peripheral’s registers hardware
+  *           - Macros to access peripheralï¿½s registers hardware
   *  
   ******************************************************************************
   * @attention
@@ -61,14 +61,6 @@
   * @{
   */
   
-/* Uncomment the line below according to the target STM32 device used in your
-   application 
-  */
-
-#if !defined (STM32F2XX) 
-  #define STM32F2XX
-#endif
-
 /*  Tip: To avoid modifying this file each time you need to switch between these
         devices, you can define the device in your toolchain compiler preprocessor.
   */
--- source/hal/ST_Micro/STM32F427_437xx/recipe.mk
+++ source/hal/ST_Micro/STM32F427_437xx/recipe.mk
@@ -1,4 +1,5 @@
 LDSCRIPT=$(ROOT_DIR)/source/hal/ST_Micro/STM32F427_437xx/ld/flash.ld
+DEF-y+=STM32F427_437xx
 INC-y+=$(ROOT_DIR)/include/hal/ST_Micro/STM32F427_437xx
 SRC-y+=$(ROOT_DIR)/source/hal/ST_Micro/STM32F427_437xx
 
@@ -7,10 +8,11 @@ OBJ-y+=tch_rtc.ko
 OBJ-y+=tch_boot.ko
 OBJ-y+=tch_gpio.ko
 OBJ-y+=tch_dma.ko
+OBJ-y+=tch_hal.ko
+
 OBJ-$(CONFIG_UART)+=tch_uart.ko
 OBJ-$(CONFIG_ADC)+=tch_adc.ko
 OBJ-$(CONFIG_SPI)+=tch_spi.ko
 OBJ-$(CONFIG_IIC)+=tch_i2c.ko
 OBJ-$(CONFIG_TIMER)+=tch_timer.ko
 
-OBJ-y+=tch_hal.ko
